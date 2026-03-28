dde747d4aca021f6223c7cb1919b2d5e25389992
Sai Shanmukh Habbar Kalle <76609011+shanmukh06@users.noreply.github.com>
2026-03-28 19:45:20 +0530

    Updated train_timit.py with new content

--- train_timit.py
+++ train_timit.py
@@ -7,7 +7,6 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
-from pytorch_lightning import Trainer
 
 import torch
 import torch.utils.data as data
@@ -21,10 +20,10 @@ def seed_torch(seed=100):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
-    pl.utilities.seed.seed_everything(seed)
+    pl.seed_everything(seed, workers=True)
 
 seed_torch()
 
@@ -32,12 +31,13 @@ from TIMIT.dataset import TIMITDataset
 from TIMIT.lightning_model_uncertainty_loss import LightningModel
 
 import torch.nn.utils.rnn as rnn_utils
+
 def collate_fn(batch):
     (seq, height, age, gender) = zip(*batch)
     seql = [x.reshape(-1,) for x in seq]
     seq_length = [x.shape[0] for x in seql]
-    data = rnn_utils.pad_sequence(seql, batch_first=True, padding_value=0)
-    return data, height, age, gender, seq_length
+    data_ = rnn_utils.pad_sequence(seql, batch_first=True, padding_value=0)
+    return data_, height, age, gender, seq_length
 
 if __name__ == "__main__":
 
@@ -56,59 +56,56 @@ if __name__ == "__main__":
     parser.add_argument('--model_type', type=str, default=TIMITConfig.model_type)
     parser.add_argument('--upstream_model', type=str, default=TIMITConfig.upstream_model)
     parser.add_argument('--narrow_band', type=str, default=TIMITConfig.narrow_band)
-    
-    parser = pl.Trainer.add_argparse_args(parser)
+
     hparams = parser.parse_args()
 
     # Check device
     if not torch.cuda.is_available():
         hparams.gpu = 0
-    else:        
+    else:
         print(f'Training Model on TIMIT Dataset\n#Cores = {hparams.n_workers}\t#GPU = {hparams.gpu}')
 
     # Training, Validation and Testing Dataset
-    ## Training Dataset
+    # Training Dataset
     train_set = TIMITDataset(
-        wav_folder = os.path.join(hparams.data_path, 'TRAIN'),
-        hparams = hparams
+        wav_folder=os.path.join(hparams.data_path, 'TRAIN'),
+        hparams=hparams
     )
-    ## Training DataLoader
+    # Training DataLoader
     trainloader = data.DataLoader(
-        train_set, 
-        batch_size=hparams.batch_size, 
-        shuffle=True, 
+        train_set,
+        batch_size=hparams.batch_size,
+        shuffle=True,
         num_workers=hparams.n_workers,
-        collate_fn = collate_fn,
+        collate_fn=collate_fn,
     )
-    ## Validation Dataset
+    # Validation Dataset
     valid_set = TIMITDataset(
-        wav_folder = os.path.join(hparams.data_path, 'VAL'),
-        hparams = hparams,
+        wav_folder=os.path.join(hparams.data_path, 'VAL'),
+        hparams=hparams,
         is_train=False
     )
-    ## Validation Dataloader
+    # Validation Dataloader
     valloader = data.DataLoader(
-        valid_set, 
+        valid_set,
         batch_size=1,
-        # hparams.batch_size, 
-        shuffle=False, 
+        shuffle=False,
         num_workers=hparams.n_workers,
-        collate_fn = collate_fn,
+        collate_fn=collate_fn,
     )
-    ## Testing Dataset
+    # Testing Dataset
     test_set = TIMITDataset(
-        wav_folder = os.path.join(hparams.data_path, 'TEST'),
-        hparams = hparams,
+        wav_folder=os.path.join(hparams.data_path, 'TEST'),
+        hparams=hparams,
         is_train=False
     )
-    ## Testing Dataloader
+    # Testing Dataloader
     testloader = data.DataLoader(
-        test_set, 
+        test_set,
         batch_size=1,
-        # hparams.batch_size, 
-        shuffle=False, 
+        shuffle=False,
         num_workers=hparams.n_workers,
-        collate_fn = collate_fn,
+        collate_fn=collate_fn,
     )
 
     print('Dataset Split (Train, Validation, Test)=', len(train_set), len(valid_set), len(test_set))
@@ -122,15 +119,20 @@ if __name__ == "__main__":
 
     model_checkpoint_callback = ModelCheckpoint(
         dirpath='checkpoints',
-        monitor='val/loss', 
+        monitor='val/loss',
         mode='min',
         verbose=1)
 
-    trainer = Trainer(
-        fast_dev_run=hparams.dev, 
-        gpus=hparams.gpu, 
-        max_epochs=hparams.epochs, 
-        checkpoint_callback=True,
+    accelerator = "cuda" if torch.cuda.is_available() else "cpu"
+    devices = hparams.gpu if (torch.cuda.is_available() and hparams.gpu and hparams.gpu > 0) else 1
+    strategy = "ddp" if (torch.cuda.is_available() and devices and devices > 1) else "auto"
+
+    trainer = pl.Trainer(
+        fast_dev_run=bool(hparams.dev),
+        accelerator=accelerator,
+        devices=devices,
+        strategy=strategy,
+        max_epochs=hparams.epochs,
         callbacks=[
             EarlyStopping(
                 monitor='val/loss',
@@ -138,16 +140,14 @@ if __name__ == "__main__":
                 patience=10,
                 verbose=True,
                 mode='min'
-                ),
+            ),
             model_checkpoint_callback
         ],
         logger=logger,
-        resume_from_checkpoint=hparams.model_checkpoint,
-        distributed_backend='ddp',
         auto_lr_find=True
-        )
+    )
 
     # Fit model
-    trainer.fit(model, train_dataloader=trainloader, val_dataloaders=valloader)
+    trainer.fit(model, train_dataloaders=trainloader, val_dataloaders=valloader, ckpt_path=hparams.model_checkpoint)
 
     print('\n\nCompleted Training...\nTesting the model with checkpoint -', model_checkpoint_callback.best_model_path)
